ff8ee6b2d93458b484fb8272920b697ee8318df8
Weirong Wu <weirongw@umich.edu>
2023-05-17 19:35:27 -0400

    updated footer section about technologies used

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,7 +19,7 @@ export function Footer() {
       <Container.Outer>
         <div className="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40">
           <Container.Inner>
-            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+            <div className="flex flex-col items-center justify-between gap-10 sm:flex-row">
               <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/articles">Blog</NavLink>
@@ -27,6 +27,8 @@ export function Footer() {
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Peter Wu. All rights reserved.
+                Built with React, JavaScript, Tailwind CSS, NEXT.js, and
+                deployed on Netlify.
               </p>
             </div>
           </Container.Inner>

94b7b73d6baa3f2da03c33fc40476efc79405ab9
Weirong Wu <weirongw@umich.edu>
2023-05-17 02:30:53 -0400

    added separators between experiences

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -182,10 +182,10 @@ function Resume() {
         <BriefcaseIcon className="h-6 w-6 flex-none" />
         <span className="ml-3">Experience</span>
       </h2>
-      <ol className="mt-6 space-y-4">
+      <ol className="mt-6 space-y-4 divide-y divide-dotted">
         {resume.map((role, roleIndex) => (
           <li key={roleIndex} className="flex gap-4">
-            <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center rounded-full shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+            <div className="relative mt-1 flex h-10 w-10 flex-none items-center justify-center">
               <a href={role.link}>
                 <Image src={role.logo} alt="" className="h-7 w-7" unoptimized />
               </a>
